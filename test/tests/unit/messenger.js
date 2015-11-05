4b0301ad3d663a2a02f84ae4375cc190fcaf33cf
Ksiusha <mksyousha@mail.ru>
2015-11-05 22:30:33 +0300

    добален тест messenger.js

---
+++ test/tests/unit/messenger.js
@@ -0,0 +1,8 @@
+var messenger = require("../../");
+var res=require("/../../mock_modules/node_modules/res.js");
+describe("messenger", function()"{
+it(#"processIndex",function(done){
+res._setup('render','index',{},done)
+messenger.processIndex({},res);
+});
+});
