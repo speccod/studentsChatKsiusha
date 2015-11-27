b177a007848c9dfe9e6bf0382554fa1547dbcdc4
Max <maxim-but@mail.ru>
2015-11-27 23:00:04 +0300

    Больше пробеловgit add test/!

--- test/tests/unit/messenger.js
+++ test/tests/unit/messenger.js
@@ -1,8 +1,9 @@
 var messenger = require("../../../lib/messenger.js");
-var res=require("../../../mock_modules/node_modules/res.js");
-describe("messenger", function(){
-	it("#processIndex",function(done){
-	res._setup('render','index',{},done);
-	messenger.processIndex({},res);
+var res = require("../../../mock_modules/node_modules/res.js");
+
+describe("messenger", function() {
+	it("#processIndex", function(done) {
+		res._setup('render', 'index', {}, done);
+		messenger.processIndex({}, res);
 	});
 });
