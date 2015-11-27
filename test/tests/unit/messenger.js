c178577a73a410830552f82412d1138b228c40ee
Ksiusha <mksyousha@mail.ru>
2015-11-27 21:59:15 +0300

    Изменен тест messenger.js

--- test/tests/unit/messenger.js
+++ test/tests/unit/messenger.js
@@ -1,8 +1,8 @@
-var messenger = require("../../");
-var res=require("/../../mock_modules/node_modules/res.js");
-describe("messenger", function()"{
-it(#"processIndex",function(done){
-res._setup('render','index',{},done)
-messenger.processIndex({},res);
-});
+var messenger = require("../../../lib/messenger.js");
+var res=require("../../../mock_modules/node_modules/res.js");
+describe("messenger", function(){
+	it("#processIndex",function(done){
+	res._setup('render','index',{},done);
+	messenger.processIndex({},res);
+	});
 });
